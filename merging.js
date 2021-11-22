2242601eeb9a0b412c09cea193c1b6b2ce2a5ce8
Anuppandey009 <anuppandey000@gmail.com>
2021-11-22 19:34:14 +0530

    mmerging

--- merging.js
+++ merging.js
@@ -2,9 +2,7 @@
 var str1='1 5 7 9'
 var str2='2 4 6 8'
 
-
-function merge(str1,str2){
-    var arr1=str1.split(" ").map(Number)
+var arr1=str1.split(" ").map(Number)
 var arr2=str2.split(" ").map(Number)
 
 
@@ -26,30 +24,11 @@ while(i<n1 && j<n2){
     }
 }
 
+for(;i<n1;i++){
+    arr.push(arr1[i])
+}
+for(;j<n2;j++){
+    arr.push(arr2[j])
+}
+
 console.log(arr.join(" "));
-}
-
-
-
-function runProgram(input){
-    var newInput = input.split("\n");
-    var str1=newInput[0]
-    var str2=newInput[1]
-    merge(str1,str2)
- 
-}
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-let read = "";
-process.stdin.on("data", function (input) {
-   read += input;
-});
-process.stdin.on("end", function () {
-   read = read.replace(/\n$/,"")
-  runProgram(read);
-});
-process.on("SIGINT", function () {
-   read = read.replace(/\n$/,"")
-   runProgram(read);
-   process.exit(0);
-});
